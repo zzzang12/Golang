ec0d400991d39472c2b49394496cc9bc6d1d7886
zzzang12 <sud0@kakao.com>
2022-05-31 23:07:00 +0900

    STYLE: 메인 메뉴 구조 코드 포맷팅 변경

--- shopping/main.go
+++ shopping/main.go
@@ -41,28 +41,28 @@ func main() {
 		fmt.Println("4. 배송 상태 확인")
 		fmt.Println("5. 장바구니 확인")
 		fmt.Println("6. 프로그램 종료")
-		fmt.Print("실행할 기능을 입력하시오 :")
+		fmt.Print("실행할 기능을 입력하시오: ")
 
 		fmt.Scanln(&menu)
 		fmt.Println()
 
 		if menu == 1 { // 물건 구매
-			fmt.Print("엔터를 입력하면 메뉴 화면으로 돌아갑니다.")
+			fmt.Println("엔터를 입력하면 메뉴 화면으로 돌아갑니다.")
 			fmt.Scanln()
 		} else if menu == 2 { // 남은 수량 확인
-			fmt.Print("엔터를 입력하면 메뉴 화면으로 돌아갑니다.")
+			fmt.Println("엔터를 입력하면 메뉴 화면으로 돌아갑니다.")
 			fmt.Scanln()
 		} else if menu == 3 { // 잔여 마일리지 확인
-			fmt.Print("엔터를 입력하면 메뉴 화면으로 돌아갑니다.")
+			fmt.Println("엔터를 입력하면 메뉴 화면으로 돌아갑니다.")
 			fmt.Scanln()
 		} else if menu == 4 { // 배송 상태 확인
-			fmt.Print("엔터를 입력하면 메뉴 화면으로 돌아갑니다.")
+			fmt.Println("엔터를 입력하면 메뉴 화면으로 돌아갑니다.")
 			fmt.Scanln()
 		} else if menu == 5 { // 장바구니 확인
-			fmt.Print("엔터를 입력하면 메뉴 화면으로 돌아갑니다.")
+			fmt.Println("엔터를 입력하면 메뉴 화면으로 돌아갑니다.")
 			fmt.Scanln()
 		} else if menu == 6 { // 프로그램 종료
-			fmt.Println("프로그램을 종료합니다.")
+			fmt.Print("프로그램을 종료합니다.")
 			return
 		} else {
 			fmt.Println("잘못된 입력입니다. 다시 입력해주세요.")
