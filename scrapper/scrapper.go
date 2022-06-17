e0bb024b5b5ec40246404093e092a65817af45d6
zzzang12 <sud0@kakao.com>
2022-06-17 14:32:34 +0900

    REFACTOR: hitURL 수정

--- scrapper/scrapper.go
+++ scrapper/scrapper.go
@@ -22,15 +22,22 @@ func main() {
 		"https://nomadcoders.co/",
 	}
 
+	results := map[string]string{}
 	for _, url := range urls {
+		result := "OK"
 		if err := hitURL(url); err != nil {
-			fmt.Println(url)
-			fmt.Println(err)
+			result = "FAILED"
 		}
+		results[url] = result
+	}
+
+	for url, result := range results {
+		fmt.Printf("%s: %s\n", url, result)
 	}
 }
 
 func hitURL(url string) error {
+	fmt.Println("Checking:", url)
 	resp, err := http.Get(url)
 	if err != nil || resp.StatusCode >= 400 {
 		return errRequestFailed
